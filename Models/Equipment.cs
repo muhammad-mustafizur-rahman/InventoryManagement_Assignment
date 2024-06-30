b23a709edddb099fd8d5e41ceb668ed8a2149ac9
Mostafizur.Rahman@coxautoinc.com <Mostafizur.Rahman@coxautoinc.com>
2024-06-30 06:28:39 -0500

    Add project files.

---
+++ Models/Equipment.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement_Assignment.Models
+{
+    public class Equipment
+    {
+        public int EquipmentID { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public DateTime EntryDate { get; set; }
+        public Member Member { get; set; }
+        public Equipment()
+        {
+            Member = new Member();
+        }
+
+        public static List<Equipment> LstEquipment()
+        {
+
+            List<Equipment> equipmentList = new List<Equipment>();
+
+
+            for (int i = 1; i < 30; i++)
+            {
+                Equipment equipment = new Equipment();
+                equipment.Name = "Computer " + i.ToString();
+                equipment.Count = i * 5;
+                equipment.EntryDate = DateTime.Now.Date;
+                equipmentList.Add(equipment);
+            }
+
+            return equipmentList;
+        }
+
+
+        public static List<Equipment> LstAssignedEquipment()
+        {
+
+            List<Equipment> equipmentList = new List<Equipment>();
+
+
+            for (int i = 1; i < 30; i++)
+            {
+                Equipment equipment = new Equipment();
+                equipment.Name = "Computer " + i.ToString();
+                equipment.Member.MemberName = "Member " + i.ToString();
+                equipment.Member.MobileNumber = "01721178207";
+                equipmentList.Add(equipment);
+            }
+
+            return equipmentList;
+        }
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement_Assignment.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string MemberName { get; set; }
+        public string MemberDesignation { get; set; }
+        public string MobileNumber { get; set; }
+
+
+        public static List<Member> LstMember()
+        {
+
+            List<Member> memberList = new List<Member>();
+
+
+            for (int i = 1; i < 30; i++)
+            {
+                Member member = new Member();
+                member.MemberName = "Member " + i.ToString();
+                member.MemberDesignation = "Software Eng";
+                member.MobileNumber = "017211782"+ i;
+                memberList.Add(member);
+            }
+
+            return memberList;
+        }
+
+    }
+}
